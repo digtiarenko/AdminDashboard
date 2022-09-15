9fded4e4a7d52d2b28841b94424cf6665990e86c
Valentyn Digtiarenko <baggins999@gmail.com>
2022-09-15 14:27:44 +0300

    15/09

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,42 @@
 import './navbar.scss';
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import MessageIcon from '@mui/icons-material/Message';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 
 export const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="search" name="" id="search" placeholder="Search..." />
+          <SearchIcon />
+        </div>
+        <ul className="navItems">
+          <li className="navItem">
+            Language
+            <LanguageIcon className="icon" />
+          </li>
+          <li className="navItem">
+            <MessageIcon className="icon" />
+            <div className="counter">1</div>
+          </li>
+          <li className="navItem">
+            <NotificationsNoneIcon className="icon" />
+            <div className="counter">2</div>
+          </li>
+          {/* <li className="navItem">
+            <LanguageIcon className="icon" />
+          </li> */}
+          <li className="navItem">
+            <img
+              src="https://gravatar.com/avatar/375fe6c10e1a14988d5bf564742bd201?s=400&d=robohash&r=x"
+              alt="avatar"
+              className="avatar"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,15 @@
 import './sidebar.scss';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import LocalAtmIcon from '@mui/icons-material/LocalAtm';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import ListIcon from '@mui/icons-material/List';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export const Sidebar = () => {
   return (
@@ -11,44 +20,62 @@ export const Sidebar = () => {
       <hr />
       <div className="center">
         <ul className="list">
+          <p className="title">MAIN</p>
           <li>
-            <DashboardIcon />
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <PeopleAltIcon />
+            <PeopleAltIcon className="icon" />
             <span>Users</span>
           </li>
           <li>
+            <ProductionQuantityLimitsIcon className="icon" />
             <span>Products</span>
           </li>
           <li>
+            <LocalAtmIcon className="icon" />
             <span>Orders</span>
           </li>
+          <p className="title">USEFULL</p>
           <li>
+            <QueryStatsIcon className="icon" />
             <span>Stats</span>
           </li>
           <li>
+            <NotificationsActiveIcon className="icon" />
             <span>Notification</span>
           </li>
+          <p className="title">SERVICE</p>
           <li>
+            <HealthAndSafetyIcon className="icon" />
             <span>System Health</span>
           </li>
           <li>
+            <ListIcon className="icon" />
             <span>Logs</span>
           </li>
           <li>
+            <SettingsSuggestIcon className="icon" />
             <span>Settings</span>
           </li>
+          <p className="title">USER</p>
           <li>
+            <PersonOutlineIcon className="icon" />
             <span>Profile</span>
           </li>
           <li>
+            <LogoutIcon className="icon" />
             <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className="bottom">color options</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   );
 };
